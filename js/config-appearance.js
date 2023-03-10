9ccd3ead2717de31fc2738079f6dc9951239e22e
Jeremy Walsh <jeremy.p.walsh@gmail.com>
2023-03-10 15:56:00 -0500

    Fixes for size of plot. Fixes for crokinole information. Fixes for colour of discs.

--- js/config-appearance.js
+++ js/config-appearance.js
@@ -1,10 +1,10 @@
 const cfgAppearance = {
     // team colors
-    blueTeam: "rgba(53, 171, 169, 0.7)",
-    orangeTeam: "rgba(234, 142, 72, 0.7)",
+    blueTeam: "rgba(0, 0, 0, 0.7)",
+    orangeTeam: "rgba(255, 0, 0, 0.7)",
     greyTeam: "rgba(170, 170, 170, 0.7)",
-    blueTeamSolid: "rgba(53, 171, 169, 1)",
-    orangeTeamSolid: "rgba(234, 142, 72, 1)",
+    blueTeamSolid: "rgba(0, 0, 0, 1)",
+    orangeTeamSolid: "rgba(255, 0, 0, 1)",
     greyTeamSolid: "rgba(170, 170, 170, 1)",
     // transition durations
     newRowDuration: 650,
